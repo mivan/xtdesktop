72c741f8f4032538bcbce42e6529cb471768bc11
Greg Pazo <gpazo@xtuple.com>
2014-02-14 16:32:33 -0500

    fix funky characters, update package.xml with correct file path

--- resources/database/misc/usersonline-type.sql
+++ resources/database/misc/usersonline-type.sql
@@ -1,14 +1,15 @@
-﻿-- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
+-- Copyright (c) 1999-2014 by OpenMFG LLC, d/b/a xTuple. 
 -- See www.xtuple.com/CPAL for the full text of the software license.
 CREATE TYPE xtdesktop.usersonline AS
-   (usr_id integer, 
-    usr_username text, 
-    usr_propername text, 
-    usr_email text, 
-    client_start timestamp with time zone, 
-    query_start timestamp with time zone, 
-    cnt_internal bigint, 
-    cnt_external bigint, 
+   (usr_id integer,
+    usr_username text,
+    usr_propername text,
+    usr_email text,
+    client_start timestamp with time zone,
+    query_start timestamp with time zone,
+    cnt_internal bigint,
+    cnt_external bigint,
     client_addr inet);
 ALTER TYPE xtdesktop.usersonline
   OWNER TO admin;
+
